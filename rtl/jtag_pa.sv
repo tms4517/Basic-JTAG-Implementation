5162d6b31629929d35e3050b20c938aa92aa8d33
tms4517 <tejminarsanghavi@hotmail.co.uk>
2024-08-03 21:29:08 +0100

    Include the state number in comments
    Easy comparison in Tb

--- rtl/jtag_pa.sv
+++ rtl/jtag_pa.sv
@@ -7,22 +7,22 @@ package jtag_pa;
 
   // TAP FSM states.
   typedef enum logic [3:0]
-  { STATE_RESET
-  , STATE_IDLE
-  , STATE_SELECT_DR
-  , STATE_SELECT_IR
-  , STATE_CAPTURE_DR
-  , STATE_CAPTURE_IR
-  , STATE_SHIFT_DR
-  , STATE_SHIFT_IR
-  , STATE_EXIT1_DR
-  , STATE_EXIT1_IR
-  , STATE_PAUSE_DR
-  , STATE_PAUSE_IR
-  , STATE_EXIT2_DR
-  , STATE_EXIT2_IR
-  , STATE_UPDATE_DR
-  , STATE_UPDATE_IR
+  { STATE_RESET      // 0
+  , STATE_IDLE       // 1
+  , STATE_SELECT_DR  // 2
+  , STATE_SELECT_IR  // 3
+  , STATE_CAPTURE_DR // 4
+  , STATE_CAPTURE_IR // 5
+  , STATE_SHIFT_DR   // 6
+  , STATE_SHIFT_IR   // 7
+  , STATE_EXIT1_DR   // 8
+  , STATE_EXIT1_IR   // 9
+  , STATE_PAUSE_DR   // 10
+  , STATE_PAUSE_IR   // 11
+  , STATE_EXIT2_DR   // 12
+  , STATE_EXIT2_IR   // 13
+  , STATE_UPDATE_DR  // 14
+  , STATE_UPDATE_IR  // 15
   } ty_STATE_TAP_FSM;
 
   localparam int unsigned IR_SCAN_CODE = 1;
