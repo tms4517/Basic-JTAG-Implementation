7ad6cf9bb935a4f1bcba0eb3a83e511be05be10a
Tej Sanghavi <41344492+tms4517@users.noreply.github.com>
2024-08-09 13:25:28 +0100

    Update jtag_instrReg.sv

--- rtl/jtag_instrReg.sv
+++ rtl/jtag_instrReg.sv
@@ -5,22 +5,30 @@ module jtag_instrReg
   , input  var logic i_trst_n
 
   , input  var logic i_stateIsUpdateIr
+  , input  var logic i_stateIsCaptureIr
 
   , input  var logic [REG_W-1:0] i_shiftReg
+
   , output var logic [REG_W-1:0] o_instrReg
   );
 
   import jtag_pa::*;
 
-  logic [REG_W-1:0] instr_q;
+  logic [REG_W-1:0] instr_d, instr_q;
 
   always_ff @(posedge i_tclk, negedge i_trst_n)
     if (!i_trst_n)
       instr_q <= '0;
-    else if (i_stateIsUpdateIr)
-      instr_q <= i_shiftReg;
     else
-      instr_q <= instr_q;
+      instr_q <= instr_d;
+
+  always_comb
+    if (i_stateIsUpdateIr)
+      instr_d = i_shiftReg;
+    else if (i_stateIsCaptureIr)
+      instr_d = IR_SCAN_CODE;
+    else
+      instr_d = instr_q;
 
   always_comb
     o_instrReg = instr_q;
